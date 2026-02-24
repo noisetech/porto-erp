44d913999c8ff5dd0442973ef337d45c603d66f0
noisetech <box92143@gmail.com>
2026-02-25 03:49:11 +0700

    persiapan rekening controller

--- app/Http/Controllers/Admin/Finance/RekeningBankController.php
+++ app/Http/Controllers/Admin/Finance/RekeningBankController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin\Finance;
 
 use App\Http\Controllers\Controller;
+use App\Models\RekeningBank;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class RekeningBankController extends Controller
 {
@@ -16,7 +19,43 @@ class RekeningBankController extends Controller
 
     public function listMasterBank(Request $request) {}
 
-    public function simpan(Request $request) {}
+    public function simpan(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'bank' => 'required',
+            'coa' => 'required',
+            'nama_rekening' => 'required',
+            'nomor_rekening' => 'required',
+            'nama_pemilik' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $rekening_bank = new RekeningBank();
+
+            DB::commit();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Data disimpan'
+            ], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
 
     public function update(Request $request) {}
 }
--- app/Models/RekeningBank.php
+++ app/Models/RekeningBank.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RekeningBank extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'rekening_bank';
+
+    protected $fillable = [
+        'bank_master_id',
+        'coa_d',
+        'nama_rekening',
+        'nama_pemilik',
+        'mata_uang'
+    ];
 }
